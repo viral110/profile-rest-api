165b008d73f4d5b81b00eba731b8591b5cd12e52
viral110 <viralvegad2001@gmail.com>
2021-09-02 13:18:04 +0530

    change get method

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test api view """
+
+    def get(self,request,format=None):
+        """ Returns a list of APIView features"""
+        an_apiview = [
+            'hello my name is viralvegad',
+            'i am study in indus University',
+
+        ]
+
+        return Response({'an_apiview':an_apiview, 'messages':"HelloWorld"})
